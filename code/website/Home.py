843932552c70265baed47921126278eec4ad1fa3
Anmol <anmolvernekar@gmail.com>
2023-08-03 00:26:46 +0530

    initial commit

---
+++ code/website/Home.py
@@ -0,0 +1,169 @@
+import streamlit as st
+import pandas as pd
+import requests
+import pickle
+import base64
+from streamlit_lottie import st_lottie
+import streamlit as st
+from PIL import Image
+
+# ************************************************ Pickling of Model ************************************************
+# In Pickling of Model : rb--> Load the model from pickle
+#                        lb--> Saving the model with pickle
+
+model_name='Model\\Model.pkl'
+with open(model_name,'rb') as file:
+    model=pickle.load(file)
+
+# *******************************************************************************************************************
+
+
+
+# Here we are setting the page configuration: page icon, page title, layout type
+st.set_page_config(page_title="CardioAware", page_icon="🫀" , layout="wide")
+
+
+
+# *******************************************************************************************************************
+# Here we are Styling the sidebar of the website
+st.markdown("""
+<style>
+    [data-testid=stSidebar] {
+        background:transparent;
+        font-size:18px;
+    }
+</style>
+""", unsafe_allow_html=True)
+
+# *******************************************************************************************************************
+
+
+
+# *******************************************************************************************************************
+
+# We are using this section, to add background-image to the website
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+# set_background('E:/Heart_Disease_Project/Code/Website/2923472.webp')
+set_background('E:/Heart_Disease_Project/Code/Website/heart illustration.jpg')
+# *******************************************************************************************************************
+
+
+
+
+
+# *******************************************************************************************************************
+# This code can be used for a Lottie animation
+# lottie = """
+# <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+# <lottie-player src="https://lottie.host/0ec1f945-d5c7-4242-acb8-aed934124f09/yoeQjUP5a8.json"  background="transparent"  speed="0"  style="width: 500px; height: 500px;"  loop  autoplay></lottie-player>
+# """s
+
+            # right: 60px;
+
+# st.components.v1.html(lottie, width=300, height=300)
+# *******************************************************************************************************************
+
+
+
+# *******************************************************************************************************************
+# Used for importing the Css file ( style.css)
+def local_css():
+    with open("E:\Heart_Disease_Project\Code\Website\Style\style.css") as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+# *******************************************************************************************************************
+
+
+
+
+def webcode():    
+    local_css()
+    with st.container():
+        original_title = '<p style="position:relative;top:-120px;color:White;font-weight:500; font-size: 32px;"><b><u>Heart Disease Prediction</u></b></p>'
+        st.markdown(original_title, unsafe_allow_html=True)
+        original_title = '<p style=" position:relative;color:white;font-weight:bold; font-size: 20px;top:-130px;padding-left:50px;padding-right:50px;">At CardioAware, we are dedicated to promoting heart health and helping individuals assess their risk of heart-disease accurately and conveniently. Our state-of-the-art platform employs artificial intelligence and machine learning algorithms to provide you with personalized and reliable predictions.</p>'
+        st.markdown(original_title, unsafe_allow_html=True)
+
+    with st.container():
+        original_title = '<p style=" position:relative;top:-140px;color:White; font-size: 32px;"><b><u>Key Features: </u></b></p>'
+        st.markdown(original_title, unsafe_allow_html=True)
+        original_title = '<p style=" position:relative;color:white;font-weight:bold; font-size: 20px;top:-133px;padding-left:50px;padding-right:50px;">Comprehensive Risk Assessment, User-Friendly Interface<br> ,Instant Results, Educational Resources..</p>'
+        st.markdown(original_title, unsafe_allow_html=True)
+
+    with st.container():
+        original_title = '<p style=" position:relative;top:-140px;color:White; font-size: 32px;"><b><u>Contact Us: </u></b></p>'
+        st.markdown(original_title, unsafe_allow_html=True)
+
+        contact_form="""
+        <form action="https://formsubmit.co/comderboi@gmail.com" method="POST">
+        <input type="hidden" name="_captcha" value="false">
+        <input type="text" name="name" placeholder="Your Name" required><input type="email" name="email" placeholder="Your email" required>
+        <textarea name="message" placeholder="Query" required></textarea>
+        <button type="submit">Send</button></form>"""
+
+        left_column ,right_column =st.columns(2)
+        with left_column:
+            st.markdown(contact_form, unsafe_allow_html=True)
+        with right_column:
+            st.empty()
+
+
+       
+        
+# *******************************************************************************************************************
+# Model copy
+# def main():
+#     age=st.slider("Age: ", 18, 100, 50)
+#     sex=st.selectbox("Sex-> (Male :- 0), (Female :- 1): ", [0,1])
+#     cp=st.slider("Chestpain: ",0,3,1)
+#     trestbps=st.slider("Resting Blood pressure: ",90,200,120)
+#     chol=st.slider("Cholesterol: ",100,600,250)
+#     fbs=st.selectbox("Fasting Blood Sugar > 120mg/dl ", [0,1])
+#     restecg=st.selectbox("Resting Electrocardiographic Results ",[0,1,2])
+#     thalach = st.slider('Maximum Heart Rate Achieved', 70, 220, 150)
+#     exang = st.selectbox('Exercise Induced Angina', [0, 1])
+#     # oldpeak = st.slider('ST Depression Induced by Exercise Relative to Rest', 0.0, 6.2, 1.0)
+#     # slope = st.slider('Slope of the Peak Exercise ST Segment', 0, 2, 1)
+#     # ca = st.slider('Number of Major Vessels Colored by Fluoroscopy', 0, 4, 1)
+#     # thal = st.slider('Thalassemia', 0, 3, 1)
+
+#     if st.button('Predict'):
+#         user_input = pd.DataFrame(data={
+#             'age': [age],
+#             'sex': [sex],
+#             'cp': [cp],
+#             'trestbps': [trestbps],
+#             'chol': [chol],
+#             'fbs': [fbs],
+#             'restecg': [restecg],
+#             'thalach': [thalach],
+#             'exang': [exang],
+#             # 'oldpeak': [oldpeak],
+#             # 'slope': [slope],
+#             # 'ca': [ca],
+#             # 'thal': [thal]
+#         })
+#         prediction = model.predict(user_input)
+#         st.write(f"Prediction: {'Positive' if prediction[0] == 1 else 'Negative'}")
+# *******************************************************************************************************************
+
+
+if __name__ == '__main__':
+    webcode()
+
---
+++ code/website/pages/Predict.py
@@ -0,0 +1,222 @@
+import streamlit as st
+import pandas as pd
+import requests
+import pickle
+from streamlit_lottie import st_lottie
+import base64
+import streamlit as st
+from PIL import Image
+import streamlit.components.v1 as components
+
+
+# ************************************************ Pickling of Model ************************************************
+# In Pickling of Model : rb--> Load the model from pickle
+#                        lb--> Saving the model with pickle
+
+model_name='Model\\Model.pkl'
+with open(model_name,'rb') as file:
+    model=pickle.load(file)
+# *******************************************************************************************************************
+
+
+# Here we are setting the page configuration: page icon, page title, layout type
+st.set_page_config(page_title="CardioAware", page_icon="🫀" , layout="wide")
+
+
+
+
+# *******************************************************************************************************************
+# This code is used for the styling of the header statement
+def header(url):
+    st.markdown(f'<p style="background-color:transparent;color:white;font-size:32px;font-weight:bolder;border-radius:2%;">{url}</p>', unsafe_allow_html=True)
+
+header("Predict your Heart Health!!")
+# ********************************************************************************
+
+
+
+
+
+# ********************************************************************************
+st.markdown("""
+        <style>
+               .block-container {
+                    padding-top: 1rem;
+                    padding-bottom: 0rem;
+                    padding-left: 5rem;
+                    padding-right: 5rem;
+                }
+        </style>
+        """, unsafe_allow_html=True)
+# ********************************************************************************
+
+# This code is used for the removal of the Header of the streamlit page
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+header{visibility: hidden;}
+</style>
+
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+# ********************************************************************************
+#                                Background Image code
+
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+set_background('E:/Heart_Disease_Project/Code/Website/heart illustration.jpg')
+
+# ********************************************************************************
+                            # Styled Text
+st.markdown("""
+<style>
+.big-font {
+    font-size:20px !important;
+    color: white;
+    font-weight:bold;
+}
+</style>
+""", unsafe_allow_html=True)
+# ********************************************************************************
+# This code is for the Predict button styling
+
+st.markdown("""
+    <style>
+    .stButton button {
+        background-color:white;
+        color: black;
+        border: 2px solid black; /* Deep Pink color */
+        border-radius: 10px;
+        padding: 10px 20px;
+        width: 30%;
+        height:30%;
+    }
+    .stButton button:hover {
+        background-color:rgb(100, 242, 90);
+        color: black;
+        border: 2px solid black; /* Deep Pink color */
+        border-radius: 10px;
+        padding: 10px 20px;
+        width: 30%;
+        height:30%;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+# ********************************************************************************
+
+
+# ********************************************************************************
+# This code is for the styling of the sidebar
+
+st.markdown("""
+<style>
+    [data-testid=stSidebar] {
+        background-color:transparent;
+        font-size:18px;
+    }
+</style>
+""", unsafe_allow_html=True)
+# ********************************************************************************
+
+
+def main():
+    col1,col2,col3,col4,col5=st.columns([0.7,1,1,1,1.1])
+    with col1:
+        st.markdown('<p class="big-font">Age </p>', unsafe_allow_html=True)
+        age=st.number_input("", 0,100)
+        st.markdown('<p class="big-font">Thalassemia</p>', unsafe_allow_html=True)
+        thal=st.selectbox("", [0,1,2,3])
+
+    with col2:
+        st.markdown('<p class="big-font">Gender </p>', unsafe_allow_html=True)
+        gender=st.selectbox("empty",['Male','Female'],label_visibility="hidden")
+        if gender=='Male':
+            sex=0
+        else:
+            sex=1
+        st.markdown('<p class="big-font">Resting BP mm/Hg</p>', unsafe_allow_html=True)
+        trestbps=st.number_input("",90,200)
+
+    with col3:
+        st.markdown('<p class="big-font">Chestpain </p>', unsafe_allow_html=True)
+        val=st.selectbox("",['None','Mild','Moderate','Severe'],label_visibility="hidden")
+        if val=='None':
+            cp=0
+        elif val=='Mild':
+            cp=1
+        if val=='Moderate':
+            cp=2
+        if val=='Severe':
+            cp=3
+
+        st.markdown('<p class="big-font">Fasting BloodSugar</p>', unsafe_allow_html=True)
+        ans=st.number_input("",0,200)
+        if ans>120:
+            fbs=1
+        else:
+            fbs=0
+
+    with col4:
+        st.markdown('<p class="big-font">Cholesterol </p>', unsafe_allow_html=True)
+        chol=st.number_input("",100,600,label_visibility='hidden')
+
+        st.markdown('<p class="big-font">Resting ECG Result</p>', unsafe_allow_html=True)
+        restecg=st.selectbox("",[0,1,2],label_visibility="hidden")
+
+    with col5:
+        st.markdown('<p class="big-font">Max Heart Rate</p>', unsafe_allow_html=True)
+        thalach=st.number_input("", 70, 220)
+
+        st.markdown('<p class="big-font">Chestpain on excercise</p>', unsafe_allow_html=True)
+        vale =st.selectbox("",['Yes','No'])
+        if vale=='Yes':
+            exang=1
+        elif vale=='No':
+            exang=0
+
+
+    
+    # oldpeak = st.slider('ST Depression Induced by Exercise Relative to Rest', 0.0, 6.2, 1.0)
+    # slope = st.slider('Slope of the Peak Exercise ST Segment', 0, 2, 1)
+    # ca = st.slider('Number of Major Vessels Colored by Fluoroscopy', 0, 4, 1)
+    # thal = st.slider('Thalassemia', 0, 3, 1)
+    st.write("##")
+
+    if st.button('Predict'):
+        user_input = pd.DataFrame(data={
+            'age': [age],
+            'sex': [sex],
+            'cp': [cp],
+            'trestbps': [trestbps],
+            'chol': [chol],
+            'fbs': [fbs],
+            'restecg': [restecg],
+            'thalach': [thalach],
+            'exang': [exang],
+            'thal': [thal]
+            # 'oldpeak': [oldpeak],
+            # 'slope': [slope],
+            # 'ca': [ca],
+        })
+        prediction = model.predict(user_input)
+        st.write(f"Heart Disease Prediction: {'Positive' if prediction[0] == 1 else 'Negative'}")
+
+if __name__ == '__main__':
+    main()
+
